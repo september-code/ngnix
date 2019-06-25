85dd8f1d5305c2d5f39d288e82531472bf712cc9
september <13329991984@163.com>
2019-06-25 16:28:25 +0800

    demo2

---
+++ test/src/main/java/com/example/test/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.example.test.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Create bySeptember
+ * 2019/6/25
+ * 16:23
+ */
+@RestController
+public class TestController {
+    @RequestMapping("/test")
+    public String test(){
+        return "test";
+    }
+}
